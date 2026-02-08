20b5a4548baac1ce8032af26f4b561eb18d2babb
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-09 03:17:08 +0530

    changes done

---
+++ users.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./common.sh
+
+APP_NAME="user"
+
+check_root
+nodejs_installation
+app_setup
+
+cp $SCRIPT_NAME/user.service /etc/systemd/system/user.service
+VALIDATE $? "Creating systemctl services"
+
+systemd_setup
