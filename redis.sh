84d98e382a73430e03fb96054be9cdcab9f44d61
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-09 04:59:37 +0530

    changes done

--- redis.sh
+++ redis.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-B="\e[34m"
-N="\e[0m"
-
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run the script with root user $N" | tee -a $LOGS_FILE
-    exit 1
-fi
-
-mkdir -p $LOGS_FOLDER
-
-VALIDATE () {
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-    fi
-}
+source ./common.sh
 
 dnf module disable redis -y &>>$LOGS_FILE
 VALIDATE $? "Disabling default redis"
