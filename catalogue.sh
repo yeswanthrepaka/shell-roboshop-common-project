c1fa6d21098ed0ded175acef3954a0d66439c619
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-09 02:40:30 +0530

    changes done

--- catalogue.sh
+++ catalogue.sh
@@ -18,15 +18,6 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
 VALIDATE $? "Installing mongodb"
 
-# INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-
-#     if [ $INDEX -le 0 ]; then
-#         mongosh --host $MONGODB_HOST </app/db/master-data.js
-#         VALIDATE $? "Loading products"
-#     else
-#         echo -e "$Y Products already loaded... SKIPPING $N"
-#     fi
-
 INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
     if [ $INDEX -le 0 ]; then
@@ -37,3 +28,5 @@ INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames()
     fi
 
 auto_restart
+
+print_total_time
---
+++ frontend.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+APP_NAME="nginx"
+
+source ./common.sh
+
+check_root
+
+dnf module disable nginx -y &>>$LOGS_FILE
+VALIDATE $? "Disabling nginx default version"
+
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling nginx 1.24 version"
+
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE $? "Installing ngnix"
+
+systemctl enable nginx &>>$LOGS_FILE
+systemctl start nginx 
+VALIDATE $? "Enabling and starting nginx"
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Removing default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+VALIDATE $? "Downloading the code"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+VALIDATE $? "Unzipping the content"
+
+rm -rf /etc/nginx/nginx.conf
+
+cp $SCRIPT_NAME/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copied the nginx conf file"
+
+auto_restart
+
+print_total_time
